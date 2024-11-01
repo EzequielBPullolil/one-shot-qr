31583b41c04044f5d5f5587a3f22225eae6f18f6
EzequielBPullolil <programacionezequielk@gmail.com>
2024-11-01 20:47:22 -0300

    (feat) Connect qr_code generation to index.html, add styles and form - Created endpoint to access to generated_qr - Add session ip for track user exit qr_code page

--- src/main.py
+++ src/main.py
@@ -1,8 +1,27 @@
-from flask import Flask, render_template
+from .services.generateQR import generate_qr
+from .services.removeQr import remove_qr
+from flask import Flask, render_template, request, send_file, session
 
 app = Flask(__name__)
 
-
+app.secret_key = "s"
 @app.get("/")
 def renderHomePage():
+    if 'ip' not in session:
+        session['ip'] = request.remote_addr
     return render_template('index.html')
+@app.route('/tmp/<path:filename>')
+def serve_image(filename):
+    return send_file(f'../tmp/{filename}')
+
+
+@app.post("/qr")
+def generateQr():
+
+    url, cap = request.form["url"], request.form["cap"]
+    
+
+    
+    qrCode_name = generate_qr(url)
+    session['qrCode_name'] = qrCode_name
+    return render_template('qr_code.html', qr_code_path=qrCode_name)
