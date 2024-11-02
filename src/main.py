0201f2f5d63bab997b81965118f5717d72e7f837
EzequielBPullolil <programacionezequielk@gmail.com>
2024-11-02 16:37:22 -0300

    (feat) Improve code to no save images in backend * Now QrCodeManager generate the qr in a 64bytes instead of save it in a folder * Remove the endpoint 'tmp' in delete and get method

--- src/main.py
+++ src/main.py
@@ -6,27 +6,17 @@ app = Flask(__name__)
 
 app.config['TMP_DIR'] = environ.get("TMP_DIR")
 app.config['URL_ENCRYPT_CODE'] = environ.get("URL_ENCRYPT_CODE")
-app.secret_key = "s"
+app.secret_key = environ.get("SECRET_KEY")
 @app.get("/")
 def renderHomePage():
     return render_template('index.html')
-    
-@app.get('/tmp/<path:filename>')
-def serve_image(filename):
-    return send_file(f'{app.config.get('TMP_DIR')}/{filename}')
-
-
-@app.delete('/tmp/<path:filename>')
-def delete_image(filename):
-    os.remove(f'{app.config.get('TMP_DIR')}/{filename}')
-    return json.jsonify({})
-
 
 @app.post("/qr")
 def generateQr():
     url, cap = request.form["url"], request.form["cap"]
     qrManager = QrCodeManager(url)
     
-    qrManager.generate_qr()
+    qrCode = qrManager.generate_qr()
 
-    return render_template('qr_code.html', qr_code_id=qrManager.get_qr_id())
+    session["qrCode"] = qrCode
+    return render_template('qr_code.html', qr_image=qrCode)
--- src/services/QrCodeManager.py
+++ src/services/QrCodeManager.py
@@ -1,5 +1,6 @@
 import qrcode
-
+from io import BytesIO
+import base64
 from flask import current_app
 from datetime import datetime
 class QrCodeManager():
@@ -9,7 +10,10 @@ class QrCodeManager():
     
     def generate_qr(self): 
         img = qrcode.make(self._path)
-        img.save(self._path)
+        buf = BytesIO()
+        img.save(buf, format="PNG")
+        qr_base64 = base64.b64encode(buf.getvalue()).decode("utf-8")
+        return qr_base64
 
     def get_qr_id(self) -> str:
         return self._file_name
