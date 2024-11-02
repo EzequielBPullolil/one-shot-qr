6d8de21ea0811a597ed2d8422de5630a62375c6e
EzequielBPullolil <programacionezequielk@gmail.com>
2024-11-02 16:44:07 -0300

    (fix) fixed qr generation - Now generates qr for specific url

--- src/services/QrCodeManager.py
+++ src/services/QrCodeManager.py
@@ -5,11 +5,10 @@ from flask import current_app
 from datetime import datetime
 class QrCodeManager():
     def __init__(self, url: str):
-        self._file_name = datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".png"
-        self._path = f'{current_app.config.get('TMP_DIR')}/{self._file_name}'
-    
+        self._url = url
+        
     def generate_qr(self): 
-        img = qrcode.make(self._path)
+        img = qrcode.make(self._url)
         buf = BytesIO()
         img.save(buf, format="PNG")
         qr_base64 = base64.b64encode(buf.getvalue()).decode("utf-8")
