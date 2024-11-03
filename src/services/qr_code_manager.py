11c12287569c8762dfbff0ac11f4789a9a8470d5
EzequielBPullolil <programacionezequielk@gmail.com>
2024-11-03 16:28:23 -0300

    (feat) We adopt pep8 name conventions -- Rename variables with snake_case -- Rename filenames with snake_case -- Fix imports -- Added docstrings to methods and class

--- src/services/qr_code_manager.py
+++ src/services/qr_code_manager.py
@@ -1,18 +1,22 @@
-import qrcode
+'''
+QrCodeManager class container
+'''
 from io import BytesIO
 import base64
-from flask import current_app
-from datetime import datetime
+import qrcode
 class QrCodeManager():
+    '''
+        QrCodeManager generate the qr code from passed url
+    '''
     def __init__(self, url: str):
         self.__url = url
-        
-    def generate_qr(self): 
+
+    def generate_qr(self)->bytes:
+        '''
+            Generates qr code using the url passed by parameter
+        '''
         img = qrcode.make(self.__url)
         buf = BytesIO()
         img.save(buf, format="PNG")
         qr_base64 = base64.b64encode(buf.getvalue()).decode("utf-8")
         return qr_base64
-
-    def get_qr_id(self) -> str:
-        return self._file_name
---
+++ src/services/url_manager.py
@@ -0,0 +1,49 @@
+'''
+_
+'''
+import threading
+import shortuuid
+from .url_encryptor import UrlEncryptor
+from ..database.Url import UrlModel
+class UrlManager:
+    '''
+        UrlManager provide url services to the Flask routes
+    '''
+    def __init__(self, app_host : str, encrypt_code: str):
+        self.__app_host = app_host
+        self.__encrypter = UrlEncryptor(encrypt_code)
+
+    def generate_qr_url(self, url: str, cap: int) -> str:
+        '''
+            Generate the url shortcut for the qr code 
+            and persit it encrypted in the DB
+
+        '''
+        encrypted_url = self.__encrypter.encrypt(url)
+        url_id = shortuuid.uuid()
+        thread = threading.Thread(
+            target=self.__persist_url_shortcut_and_cap,
+            name="one_shot_qr",
+            args=(url_id, encrypted_url, cap)
+        )
+        thread.start()
+        return self.__generate_shortcut_url(url_id)
+
+    def __generate_shortcut_url(self, uuid: str) -> str:
+        print(f'{self.__app_host}/shortcut/{uuid}')
+        return f'{self.__app_host}/shortcut/{uuid}'
+
+
+    def find_real_url(self, uuid: str) -> (str, int):
+        '''
+            Provides the actual decrypted URL and URL limit associated with url_uuid
+        '''
+        (encrypted_url_str, cap) = UrlModel.read_and_decrease_cap(uuid)
+        return (self.__encrypter.decrypt(encrypted_url_str), cap)
+
+
+    def __persist_url_shortcut_and_cap(self,url_uuid, encrypted_url, cap: int):
+        '''
+            Persist the url in the db
+        '''
+        UrlModel.create(url_uuid, encrypted_url, cap)
